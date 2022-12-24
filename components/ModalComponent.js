96f995ae2a454948582da2c9fbd00af603973018
Bohdan-Dubravin <dubravin111@gmail.com>
2022-12-24 10:21:48 +0200

    change modal

--- components/ModalComponent.js
+++ components/ModalComponent.js
@@ -1,12 +1,26 @@
-import { Button, IconButton, Paper } from "@mui/material";
-import { useDispatch, useSelector } from "react-redux";
-import { hideModal } from "../redux/slices/modalSlice";
-import Modal from "@mui/material/Modal";
-import CloseIcon from "@mui/icons-material/Close";
+import { IconButton, Paper } from '@mui/material';
+import { useDispatch, useSelector } from 'react-redux';
+import { hideModal } from '../redux/slices/modalSlice';
+import Modal from '@mui/material/Modal';
+import CloseIcon from '@mui/icons-material/Close';
+import PhotoCamera from '@mui/icons-material/PhotoCamera';
+import { useState } from 'react';
 
 const ModalComponent = () => {
   const dispatch = useDispatch();
   const { show } = useSelector((state) => state.modal);
+  const [picture, setPicture] = useState(null);
+
+  const addPicture = (e) => {
+    const reader = new FileReader();
+    if (e.target.files[0]) {
+      reader.readAsDataURL(e.target.files[0]);
+    }
+
+    reader.onload = (readerEvent) => {
+      setPicture(readerEvent.target.result);
+    };
+  };
 
   return (
     <Modal
@@ -16,15 +30,48 @@ const ModalComponent = () => {
       aria-labelledby="modal-modal-title"
       aria-describedby="modal-modal-description"
     >
-      <Paper className="absolute top-[50%] left-[50%] -translate-x-2/4 -translate-y-1/2 w-[400px] p-4">
+      <Paper className="flex flex-col absolute top-[50%] left-[50%] -translate-x-2/4 -translate-y-1/2 max-w-[400px] text-center p-4 h-[400px]">
         <IconButton
           color="error"
           aria-label="delete"
           onClick={() => dispatch(hideModal())}
+          className="absolute -top-2 -right-2"
         >
           <CloseIcon />
         </IconButton>
-        <p>ADD sothing</p>
+        <div className="">
+          {picture ? (
+            <img
+              className="w-[300px] object-contain cursor-pointer"
+              src={picture}
+              alt="upload picture"
+              onClick={() => setPicture(null)}
+            />
+          ) : (
+            <IconButton
+              aria-label="upload picture"
+              color="error"
+              component="label"
+            >
+              <PhotoCamera className="w-10 h-10" />
+              <input
+                onChange={(e) => addPicture(e)}
+                hidden
+                accept="image/*"
+                multiple
+                type="file"
+              />
+            </IconButton>
+          )}
+        </div>
+        <p className="font-semibold">Upload a photo</p>
+        <form className="flex items-center p-4">
+          <input
+            type="text"
+            placeholder="Add a description..."
+            className="border-none flex-1 outline-none"
+          />
+        </form>
       </Paper>
     </Modal>
   );
--- components/Post.js
+++ components/Post.js
@@ -1,32 +1,24 @@
-import { Avatar } from "@mui/material";
-import MoreHorizIcon from "@mui/icons-material/MoreHoriz";
-import Image from "next/image";
-import SendOutlinedIcon from "@mui/icons-material/SendOutlined";
-import BookmarkBorderOutlinedIcon from "@mui/icons-material/BookmarkBorderOutlined";
-import FavoriteBorderOutlinedIcon from "@mui/icons-material/FavoriteBorderOutlined";
-import FavoriteIcon from "@mui/icons-material/Favorite";
-import SmsOutlinedIcon from "@mui/icons-material/SmsOutlined";
-import SentimentSatisfiedAltOutlinedIcon from "@mui/icons-material/SentimentSatisfiedAltOutlined";
+import { Avatar } from '@mui/material';
+import MoreHorizIcon from '@mui/icons-material/MoreHoriz';
+import Image from 'next/image';
+import SendOutlinedIcon from '@mui/icons-material/SendOutlined';
+import BookmarkBorderOutlinedIcon from '@mui/icons-material/BookmarkBorderOutlined';
+import FavoriteBorderOutlinedIcon from '@mui/icons-material/FavoriteBorderOutlined';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import SmsOutlinedIcon from '@mui/icons-material/SmsOutlined';
+import SentimentSatisfiedAltOutlinedIcon from '@mui/icons-material/SentimentSatisfiedAltOutlined';
 const Post = ({ data }) => {
   const { id, username, userImg, img, caption } = data;
 
   return (
     <div className="bg-white rounded-sm border my-7">
       <div className="flex items-center p-5">
-        <Avatar
-          src={userImg}
-          alt={username}
-          className="mr-2"
-        />
+        <Avatar src={userImg} alt={username} className="mr-2" />
         <p className="flex-1 font-bold">{username}</p>
         <MoreHorizIcon className="cursor-pointer" />
       </div>
       <div className="relative w-full h-[500px]">
-        <Image
-          src={userImg}
-          fill
-          className="object-cover"
-        />
+        <Image src={userImg} fill className="object-cover" />
       </div>
       <div className="flex justify-between p-4">
         <div className="flex space-x-4">
